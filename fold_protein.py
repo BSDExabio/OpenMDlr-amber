a6c7605bf779930d7729f1a22dede898d1435687
jkwoods <jesskw729@gmail.com>
2020-02-20 16:09:29 -0500

    moving code

--- fold_protein.py
+++ fold_protein.py
@@ -15,28 +15,27 @@
 import sys
 import subprocess
 import json
-import Bio.PDB
 import numpy as np
 import math
 import warnings
-from Bio import BiopythonWarning
+from Bio import PDB, BiopythonWarning
 
 
 with open('fold_parameters.json') as json_file:
     data = json.load(json_file)
     name = data['name']
     fasta = data['fasta']
-    distance_rst = data['distanceRstFile']
-    distance_force = data['distanceForce']
-    torsion_rst = data['angleRstFile']
-    torsion_force = data['angleForce']
-    temp = data['temp']
+    distance_rst = data['distance_restraints_filepath']
+    distance_force = data['distance_force']
+    torsion_rst = data['torsion_restraints_filepath']
+    torsion_force = data['torsion_force']
+    temp = data['temperature']
     annealing_runs = int(data['cycles'])
     mpi_prefix = data["mpi"]
     forcefield = data["forcefield"]
+    solvate = data["solvate"]
 
-
-print("Reading Sequence ...")
+print("======================== READING FASTA ========================")
 #open and read FASTA
 f = open(fasta, "r")
 lines = f.readlines()
@@ -48,8 +47,6 @@ for l in lines:
 f.close()
 
 seq = seq.replace("\n", "") #clean
-
-print(seq)
 
 #generate sequence of triples
 def tri(x):
@@ -85,7 +82,7 @@ for i,s in enumerate(seq):
         triseq = triseq + tri(s) + " "
 triseq = triseq + "}"
 
-print("Sequence is: "+ str(triseq))
+print("PROTEIN SEQUENCE: "+ str(triseq))
 
 #generate Amber Tools helper file
 subprocess.call('cp '+forcefield+' amberscript', shell=True)
@@ -96,12 +93,12 @@ h.write("\n"+name+" = sequence "+triseq+"\nsaveoff "+name+" linear.lib\nsavepdb 
 
 h.close()
 
-print("Generating linear peptide ...")
+print("=============== GENERATING LINEAR PDB WITH TLEAP ==============")
 
 #call Amber Tools tleap
 subprocess.call('tleap -s -f amberscript', shell=True)
 
-print("Amber paramaters and linear file generated")
+print("===== READING USER RESTRAINTS AND MATCHING WITH LINEAR PDB ====")
 
 #extract correct atom indices from amber linear file
 with warnings.catch_warnings():
@@ -115,7 +112,6 @@ with warnings.catch_warnings():
                     #weird Amber linear naming conversions
                     if (resname == "HID") or (resname == "HIE") or (resname == "HIP"): resname = "HIS"
                     linear_serials.update({(res.id[1], resname, atom.id) : atom.serial_number})
-print("Reading user restraints and matching with linear file ....")
 
 #make restraints given user input
 first = 1
@@ -143,12 +139,13 @@ with open(distance_rst) as input_file:
             else:
                 d.write(" &rst\n  ixpk= 0, nxpk= 0, iat= %i, %i, r1= %.2f, r2= %.2f, r3= %.2f, r4= %.2f,  /\n" % (atom1_index, atom2_index, r1, r2, r3, r4))
         except KeyError:
-                raise Exception("Mismatch detected between residue sequences")
+                raise Exception("MISMATCH BETWEEN LINEAR FILE AND RESTRAINTS INDEX")
 d.close()
-print("Distance Restraints file successfully generated")
+print("DISTANCE RESTRAINTS CORRECTLY GENERATED")
 
 #angle restraints - we use AmberTools built in makeANG_RST program
-print("Making Angle Restraints ... make sure the AmberTools makeANG_RST finds all its libraries and runs correctly")
+print("=================== MAKING ANGLE RESTRAINTS ===================")
+print("Make sure the AmberTools makeANG_RST finds all its libraries and runs correctly")
 subprocess.call('makeANG_RST -pdb linear.pdb -con '+torsion_rst+' -lib tordef.lib > RST.angles', shell=True)
 # replace force constant
 subprocess.call("sed -i 's/rk2 =   2.0, rk3 =   2.0/rk2 =   "+str(torsion_force[0])+", rk3 =   "+str(torsion_force[0])+"/g' RST.angles", shell=True)
@@ -171,9 +168,11 @@ lines = "".join(lines)
 s.write(lines)
 s.close()
 
-print("Running Minimization ...")
+print("===================== RUNNING MINIMIZATION ====================")
 subprocess.call(mpi_prefix+"sander -O -i min.in -o min.out -p prmtop -c rst7 -r min.ncrst", shell=True)
-print('Running Simulated Annealing cycle #1, distance force constant = '+str(distance_force[0])+', angle force constant = '+str(torsion_force[0])+', temperature = '+str(temp[0])+'K')
+
+print("================= RUNNING SIMULATED ANNEALING =================")
+print('SIMULATED ANNEALING CYCLE #1, DISTANCE FORCE CONSTANT = '+str(distance_force[0])+', ANGLE FORCE CONSTANT = '+str(torsion_force[0])+', TEMPERATURE = '+str(temp[0])+'K')
 subprocess.call(mpi_prefix+"sander -O -i siman.in -p prmtop -c min.ncrst -r siman1.ncrst -o siman1.out -x siman1.nc", shell=True)
 
 j = 1
@@ -208,10 +207,16 @@ for i in range(1, annealing_runs):
         s.write(lines)
         s.close()
 
-    print('Running Simulated Annealing cycle #'+str(j)+', distance force constant = '+str(dfc)+', angle force constant = '+str(tfc)+', temperature = '+str(tp)+'K')
+    print('SIMULATED ANNEALING CYCLE #'+str(j)+', DISTANCE FORCE CONSTANT = '+str(dfc)+', ANGLE FORCE CONSTANT = '+str(tfc)+', TEMPERATURE = '+str(tp)+'K')
     subprocess.call(mpi_prefix+"sander -O -i siman.in -p prmtop -c siman"+str(i)+".ncrst -r siman"+str(j)+".ncrst -o siman"+str(j)+".out -x siman"+str(j)+".nc", shell=True)
 
-print("Writing Final pdb ...")
+print("========================== SOLVATING ==========================")
+if (solvate == ""):
+    print("Solvating not currently set.")
+else:
+    #TODO: Amber solvation code
+
+print("====================== WRITING FINAL PDB ======================")
 subprocess.call("ambpdb -p prmtop -c siman"+str(j)+".ncrst > "+name+"_final.pdb", shell=True)
 
-print("Complete")           
+print("=========================== COMPLETE ==========================")           
